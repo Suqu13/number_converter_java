cf5dc100962349128163ed05b6bbeddb371cc381
jakub_garstka <jgarstka3@gmail.com>
2019-11-09 16:02:19 +0100

    feat/ initial commit

---
+++ src/main/java/garstka/jakub/NumberConverterApplication.java
@@ -0,0 +1,11 @@
+package garstka.jakub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberConverterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NumberConverterApplication.class, args);
+    }
+}
---
+++ src/test/java/garstka/jakub/NumberConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package garstka.jakub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
