9e86ae4e430c33f670fe339628421334d66c095a
jakub_garstka <jgarstka3@gmail.com>
2019-11-09 16:05:33 +0100

    feat/add constants values

---
+++ src/main/java/garstka/jakub/config/Constants.java
@@ -0,0 +1,7 @@
+package garstka.jakub.config;
+
+public class Constants {
+    public static final String NOT_IN_THE_RANGE_MESSAGE = "Provided value have to be in the range %s - %s";
+    public static final Integer LOWEST_ROMAIN_NUMBER = 1;
+    public static final Integer HIGHEST_ROMAIN_NUMBER = 3999;
+}
